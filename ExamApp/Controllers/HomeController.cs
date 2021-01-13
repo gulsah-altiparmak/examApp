a10cc60f960e1c04d49fb6af0ad840fbc5930001
gulsah-altiparmak <42993752+gulsah-altiparmak@users.noreply.github.com>
2021-01-14 01:16:57 +0300

    "test Html Agility Pack "

--- ExamApp/Controllers/HomeController.cs
+++ ExamApp/Controllers/HomeController.cs
@@ -7,23 +7,41 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ExamApp.Models;
 using Data;
+using System.Net.Http;
+using HtmlAgilityPack;
 
 namespace ExamApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly DataContext _context; 
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger,DataContext context)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            HttpClient client = new HttpClient();
+            string title;
+            // get answer in non-blocking way
+            using (var response = await client.GetAsync("https://www.wired.com/story/these-adorable-fish-robots-form-schools-like-the-real-thing/"))
+            {
+                using (var content = response.Content)
+                {
+                    // read answer in non-blocking way
+                    var result = await content.ReadAsStringAsync();
+                    var document = new HtmlDocument();
+                    document.LoadHtml(result);
+                    var node = document.DocumentNode.SelectSingleNode("//*[@id='main-content']/article/div[1]/header/div/div[1]/h1");
+                     title=node.InnerText;
+                    //Some work with page....
+                }
+            }
+            return View(title);
         }
 
         public IActionResult Privacy()
